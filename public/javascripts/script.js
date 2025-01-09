aa10f1abddb1700cab7d5f4a9ccacf1ec5f08bfc
iamshbr <shabbirzohair@outlook.com>
2025-01-09 02:11:46 +0100

    Removing Comments

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -22,17 +22,7 @@ let currentAccount;
 let sorted = false;
 
 /////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
+////////////////////////////////////////////////
 /////////////////////////////////////////////////
 
 // Login Event Listener
